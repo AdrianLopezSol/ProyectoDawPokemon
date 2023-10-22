e196ed355bdf3c1dca11dd0dca51a8107ac0f732
AdrianLopezSol <adrian1000000@hotmail.es>
2023-10-22 20:15:21 +0200

    Added DBUtil and Exceptions

---
+++ proyectoPokemon/src/main/java/com/adrianLopez/proyectoPokemon/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.adrianLopez.proyectoPokemon.exception;
+
+public class ResourceNotFoundException extends RuntimeException{
+    private static final String DESCRIPTION = "Resource not found";
+
+    public ResourceNotFoundException(String message) {
+        super(DESCRIPTION + ". " + message);
+    }
+}
